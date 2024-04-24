5ddf9c3e9babaf71f2da27b68ffe8a94b11c29de
Umka-dev <design.tvin@gmail.com>
2024-04-24 10:53:50 +0200

    add solution

--- love-calculator/app.js
+++ love-calculator/app.js
@@ -3,4 +3,4 @@ prompt("What is the second person name?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore)+1;
-alert("Your love score is " + loveScore + "%")
+alert("Your love score is " + loveScore + "%");
