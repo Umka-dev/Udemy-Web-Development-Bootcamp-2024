16b03e6fe8f6a5fd39e8fbceed3fe06b62c5c1a7
Umka-dev <design.tvin@gmail.com>
2024-05-31 13:20:18 +0200

    add time condition for rendering

--- conditional-rendering-and-ternary-operator/src/components/App.jsx
+++ conditional-rendering-and-ternary-operator/src/components/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import Login from "./Login";
+import React from 'react';
+import Login from './Login';
 
 var isLoggedIn = false;
 
+const currentTime = new Date(2024, 5, 31, 19).getHours();
+console.log(currentTime);
+
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className='container'>
+      {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+      {currentTime > 18 ? <h1>Why are you still working?</h1> : null}
+    </div>
   );
 }
 
