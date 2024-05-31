6f96b29a530bef95cf417b8c38791454cee45221
Umka-dev <design.tvin@gmail.com>
2024-05-31 16:45:16 +0200

    add pretier fixes

--- conditional-rendering-and-ternary-operator/src/components/Form.jsx
+++ conditional-rendering-and-ternary-operator/src/components/Form.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Input from "./Input";
+import React from 'react';
+import Input from './Input';
 
 function Form(props) {
   return (
-    <form className="form">
-      <Input type="text" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
+    <form className='form'>
+      <Input type='text' placeholder='Username' />
+      <Input type='password' placeholder='Password' />
       {!props.isRegistered && (
-        <Input type="password" placeholder="Confirm Password" />
+        <Input type='password' placeholder='Confirm Password' />
       )}
-      <button type="submit">
-        {props.isRegistered ? "Login" : "Registered"}
+      <button type='submit'>
+        {props.isRegistered ? 'Login' : 'Registered'}
       </button>
     </form>
   );
