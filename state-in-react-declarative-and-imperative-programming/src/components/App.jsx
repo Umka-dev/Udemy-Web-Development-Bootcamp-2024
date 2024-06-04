b62a9d87a01f8171084daa4179534e55dda0523f
Umka-dev <design.tvin@gmail.com>
2024-06-04 15:29:24 +0200

    add imperative programming solution

--- state-in-react-declarative-and-imperative-programming/src/components/App.jsx
+++ state-in-react-declarative-and-imperative-programming/src/components/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React from 'react';
+
+// Declarative Programming and States
+// function App() {
+//   var isDone = false;
+//   const strikeThrough = { textDecoration: "line-through" };
+//   return <p style={isDone ? strikeThrough : null}>Buy milk</p>;
+// }
+
+// Imperative programming
+function strike() {
+  document.getElementById('root').style.textDecoration = 'line-through';
+}
+
+function unStrike() {
+  document.getElementById('root').style.textDecoration = null;
+}
 
 function App() {
-  var isDone = false;
-  const strikeThrough = { textDecoration: "line-through" };
-  return <p style={isDone ? strikeThrough : null}>Buy milk</p>;
+  return (
+    <div>
+      <p>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
+  );
 }
 
 export default App;
