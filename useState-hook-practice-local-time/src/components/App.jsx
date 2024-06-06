fa6ff0fce7912adbde7f1975674d5b325cd732f4
Umka-dev <design.tvin@gmail.com>
2024-06-06 16:43:24 +0200

    add useState hook and setInterval to refresh current time

--- useState-hook-practice-local-time/src/components/App.jsx
+++ useState-hook-practice-local-time/src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [myTime, setTime] = useState(time);
+
+  function getTime() {
+    setTime(() => new Date().toLocaleTimeString());
+  }
+  setInterval(getTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
