e7be6c06a5c85c393ed625f2de4c7fb88c34c751
Umka-dev <design.tvin@gmail.com>
2024-06-10 19:24:38 +0200

    add my task solution using state and inline styles

--- event-handling-using-states-ternary-operator-inline-styles/src/components/App.jsx
+++ event-handling-using-states-ternary-operator-inline-styles/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+
+  function handleClick() {
+    setHeadingText('Submited');
+  }
+
+  const [mystyle, setMyStyle] = useState({ backgroundColor: 'white' });
+
+  function hoverIt() {
+    setMyStyle({ backgroundColor: 'black' });
+  }
+
+  function unHoverIt() {
+    setMyStyle({ backgroundColor: 'white' });
+  }
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+    <div className='container'>
+      <h1>{headingText}</h1>
+      <input type='text' placeholder="What's your name?" />
+      <button
+        style={mystyle}
+        onClick={handleClick}
+        onMouseOver={hoverIt}
+        onMouseOut={unHoverIt}
+      >
+        Submit
+      </button>
     </div>
   );
 }
