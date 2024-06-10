10c5215d5402c0dc6ea686bf7495ae229254e0c0
Umka-dev <design.tvin@gmail.com>
2024-06-10 16:47:40 +0200

    add start code with an example

---
+++ destructuring-practice/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import animals from "./data";
+
+console.log(animals);
+// const [name, sound] = ["cat", "meow"];
+const [cat, dog] = animals;
+console.log(cat);
+console.log(dog);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
