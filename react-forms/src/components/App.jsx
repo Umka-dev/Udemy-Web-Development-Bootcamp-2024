0453d1c3fce312545cc3295f75efa1d782c8427c
Umka-dev <design.tvin@gmail.com>
2024-06-11 14:47:06 +0200

    change to better task solution

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -2,21 +2,19 @@ import React, { useState } from 'react';
 
 function App() {
   const [name, setName] = useState('');
-  const [isButtonClicked, setButtonClicked] = useState(false);
+  const [headingText, setHeading] = useState('');
 
   function handleClick() {
-    setButtonClicked(true);
+    setHeading(name);
   }
 
   function handleChange(event) {
     console.log(event.target.value);
-    // console.log(event.target.placeholder);
-    // console.log(event.target.type);
     setName(event.target.value);
   }
   return (
     <div className='container'>
-      <h1>Hello {isButtonClicked && name + '!'}</h1>
+      <h1>Hello {headingText}</h1>
       <input
         onChange={handleChange}
         type='text'
