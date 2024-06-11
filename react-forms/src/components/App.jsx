d817bb34508256f92accfede8f5303b7e68d71dc
Umka-dev <design.tvin@gmail.com>
2024-06-11 14:37:01 +0200

    add onChange event to input and change h1 with handleClick function

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [isButtonClicked, setButtonClicked] = useState(false);
+
+  function handleClick() {
+    setButtonClicked(true);
+  }
+
   function handleChange(event) {
     console.log(event.target.value);
     // console.log(event.target.placeholder);
     // console.log(event.target.type);
+    setName(event.target.value);
   }
   return (
     <div className='container'>
-      <h1>Hello </h1>
+      <h1>Hello {isButtonClicked && name + '!'}</h1>
       <input
         onChange={handleChange}
         type='text'
         placeholder="What's your name?"
+        value={name}
       />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
