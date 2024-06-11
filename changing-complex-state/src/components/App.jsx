53fe41ad9f4bd5fd938e1840e72c41919a993d58
Umka-dev <design.tvin@gmail.com>
2024-06-11 15:51:20 +0200

    add my solution

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [headingText, setHeading] = useState('');
+
+  function handleClick(event) {
+    // setHeading(name + " " + lastName);
+    event.preventDefault();
+  }
+
+  function handleChangeFN(event) {
+    setName(event.target.value);
+  }
+
+  function handleChangeLN(event) {
+    setLastName(event.target.value);
+  }
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+    <div className='container'>
+      <h1>Hello {name + ' ' + lastName}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChangeFN}
+          type='text'
+          name='fName'
+          placeholder='First Name'
+          value={name}
+        />
+        <input
+          onChange={handleChangeLN}
+          type='text'
+          name='lName'
+          placeholder='Last Name'
+          value={lastName}
+        />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
