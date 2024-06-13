fba109b4d49655198135d710096fb51c02aff7f9
Umka-dev <design.tvin@gmail.com>
2024-06-13 15:20:11 +0200

    add solution

--- spread-operator-practice-todo-list/src/components/App.jsx
+++ spread-operator-practice-todo-list/src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setInputText(value);
+  }
+
+  function handleClick() {
+    if (inputText.trim() !== '') {
+      setItems((prevItems) => [...prevItems, inputText]);
+      setInputText('');
+    }
+  }
+
   return (
-    <div className="container">
-      <div className="heading">
+    <div className='container'>
+      <div className='heading'>
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" />
-        <button>
+      <div className='form'>
+        <input onChange={handleChange} value={inputText} type='text' />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
