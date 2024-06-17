3a646c53d9257215b1292e8768f7afa5a10870e3
Umka-dev <design.tvin@gmail.com>
2024-06-17 14:33:24 +0200

    add text stiling related to user click action

--- managing-component-tree/src/components/ToDoItem.jsx
+++ managing-component-tree/src/components/ToDoItem.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function ToDoItem(props) {
-  return <li>{props.text}</li>;
+  const [clicked, setClicked] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+
+  function handleClick() {
+    setClickCount((prevCount) => {
+      const newCount = prevCount + 1;
+      setClicked(newCount % 2 === 1);
+      return newCount;
+    });
+  }
+
+  return (
+    <div>
+      <li
+        style={{ textDecoration: clicked ? 'line-through' : 'none' }}
+        onClick={handleClick}
+      >
+        {props.text}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItem;
