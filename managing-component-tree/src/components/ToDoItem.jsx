8706a1b2ded38e9feef9d177e1141e062a77cfb4
Umka-dev <design.tvin@gmail.com>
2024-06-17 16:09:35 +0200

    replace line-through li item text to delete item from list

--- managing-component-tree/src/components/ToDoItem.jsx
+++ managing-component-tree/src/components/ToDoItem.jsx
@@ -1,22 +1,9 @@
-import React, { useState } from 'react';
+import React from "react";
 
 function ToDoItem(props) {
-  const [clicked, setClicked] = useState(false);
-
-  function handleClick() {
-    setClicked((prevValue) => {
-      return !prevValue;
-    });
-  }
-
   return (
-    <div>
-      <li
-        style={{ textDecoration: clicked ? 'line-through' : 'none' }}
-        onClick={handleClick}
-      >
-        {props.text}
-      </li>
+    <div onClick={() => props.onChecked(props.id)}>
+      <li>{props.text}</li>
     </div>
   );
 }
