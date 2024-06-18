b9b715ad673ec8ccbcec48f1172d268ad8e5d4ce
Umka-dev <design.tvin@gmail.com>
2024-06-18 12:36:10 +0200

    add solution-1

--- keeper-app-part-3/src/components/CreateArea.jsx
+++ keeper-app-part-3/src/components/CreateArea.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    id: "",
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        id: prevNote.id || uuidv4(),
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    event.preventDefault();
+    if (
+      note.title.trim() === ""
+      // || note.content.trim() === ""
+    ) {
+      return;
+    }
+    props.onAdd(note);
+    setNote({
+      id: "",
+      title: "",
+      content: "",
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={note.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          onChange={handleChange}
+          value={note.content}
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
